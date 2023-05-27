45dd744e94ffe01bb5ffd2fb4295b5b19fdd4fe2
hyeji07 <zindex_ann@naver.com>
2023-05-27 10:22:05 +0900

    Login폴더 추가

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { AiOutlineSmile } from "react-icons/ai";
+
+export default function Login() {
+  return (
+    <div>
+      <AiOutlineSmile />
+    </div>
+  );
+}
