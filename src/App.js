ee6b694b9b02bd03d3676564b950f723104885d1
yxxnhx <tjdbsgk514@hanmail.net>
2023-05-29 14:22:23 +0900

    감정일기 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ResetStyles from "./styles/ResetStyles";
-import MyJourney from "./pages/MyJourney";
 import NavBar from "./components/common/NavBar";
+import MyJourney from "./pages/MyJourney";
+import Diary from "./pages/Diary";
+import DiaryWrite from "./components/views/Diary/DiaryWrite";
+import DiaryDetail from "./components/views/Diary/DiaryDetail";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
+          <Route path="/diary" element={<Diary />} />
+          <Route path="/diary/:id" element={<DiaryDetail />} />
+          <Route path="/diary/write/:date" element={<DiaryWrite />} />
           <Route path="/my_journey" element={<MyJourney />} />
         </Routes>
       </BrowserRouter>
