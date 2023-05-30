4a148d135bb62c91df9755c8986eae888ac83346
hyeji07 <zindex_ann@naver.com>
2023-05-31 00:31:12 +0900

    feat: SelfDiagnosis 자가진단 페이지 생성 및 UI구현

---
+++ src/pages/SelfDiagnosis/index.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const SelfDiagnosis = () => {
+  const handleSubmit = () => {};
+
+  return (
+    <DiagnosisContainer>
+      <TitleContainer>
+        <MainTitle>오늘 박나나님의 감정은 어떤가요?</MainTitle>
+        <SubTitle>당신의 기분을 가장 잘 나타내는 것으로 골라주세요</SubTitle>
+      </TitleContainer>
+
+      <EmotionContainer>
+        <button>
+          <img src="" alt="" />
+        </button>
+      </EmotionContainer>
+
+      <SubMitButton onClick={handleSubmit}>자가 진단 완료</SubMitButton>
+    </DiagnosisContainer>
+  );
+};
+
+export default SelfDiagnosis;
+
+const DiagnosisContainer = styled.div``;
+const TitleContainer = styled.div`
+  text-align: center;
+  padding-top: 3rem;
+  line-height: 1.6;
+  padding-bottom: 3rem;
+`;
+const MainTitle = styled.div`
+  font-weight: 700;
+  font-size: 1.8rem;
+`;
+const SubTitle = styled.div`
+  font-size: 1.4rem;
+`;
+const EmotionContainer = styled.div``;
+
+const SubMitButton = styled.div`
+  width: 19.5rem;
+  height: 4.5rem;
+  text-align: center;
+  color: #fff;
+  font-weight: 700;
+  font-size: 1.4rem;
+  line-height: 4.5rem;
+  background: var(--sub-green-color);
+  border-radius: 4rem;
+  margin: 0 auto;
+  margin-top: 3rem;
+`;
