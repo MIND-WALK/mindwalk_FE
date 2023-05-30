b30f5f1c6647a7803493f515df642d72a834e418
hyeji07 <zindex_ann@naver.com>
2023-05-31 01:15:07 +0900

    feat: SelfDiagnosis 자가진단 페이지 구현_API적용전 세팅

--- src/pages/SelfDiagnosis/index.jsx
+++ src/pages/SelfDiagnosis/index.jsx
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRecoilState } from "recoil";
 import { styled } from "styled-components";
 
 const SelfDiagnosis = () => {
-  const handleSubmit = () => {};
+  // const [authenticated, setAuthenticated] = useRecoilState(userIdState);
+  // const url = process.env.REACT_APP_API_URL;
+
+  // const userId = localStorage.getItem("id");
+
+  const [selfEmotion, setSelfEmotion] = useState("");
+
+  const emotionLists = [
+    { emotion: "" },
+    { emotion: "" },
+    { emotion: "" },
+    { emotion: "" },
+    { emotion: "" },
+  ];
+
+  /* 
+  const handleSubmit = async () => {
+    // api적용전 세팅
+    try {
+      const response = await axios.post(
+        `${url}/user/info/${userId}`,
+        { self: `${selfEmotion}` },
+        {},
+      );
+
+      // const { ts, value } = response.data.self;//?
+      // console.log(ts, value);
+
+      // 페이지이동
+      if (loginData.data.code === 200) {
+        // ? navigate("/");
+        // ? window.location.href = '/new-page';
+      }
+    } catch (error) {
+      console.error(error);
+      alert("오류가 발생했습니다.");
+    }
+  }; */
 
   return (
     <DiagnosisContainer>
       <TitleContainer>
         <MainTitle>오늘 박나나님의 감정은 어떤가요?</MainTitle>
-        <SubTitle>당신의 기분을 가장 잘 나타내는 것으로 골라주세요</SubTitle>
+        <SubTitle>당신의 기분을 가장 잘 나타내는 것으로 1개만 골라주세요</SubTitle>
       </TitleContainer>
 
       <EmotionContainer>
-        <button>
-          <img src="" alt="" />
-        </button>
+        {emotionLists.map((item, index) => (
+          <button onClick={() => setSelfEmotion(item.emotion)} key={index}>
+            {/*   <img
+              src={require(`./../../assets/img/${item.emotion}.png`).default}
+              alt={item.emotion}
+            /> */}
+          </button>
+        ))}
       </EmotionContainer>
 
-      <SubMitButton onClick={handleSubmit}>자가 진단 완료</SubMitButton>
+      <SubMitButton /*  onClick={handleSubmit} */>자가 진단 완료</SubMitButton>
     </DiagnosisContainer>
   );
 };
