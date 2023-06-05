32623dda9c2d7824f078c57f3fff57138ad74017
hyeji07 <zindex_ann@naver.com>
2023-06-05 18:26:50 +0900

    feat: 챌린지 api 연결, 함수 구현진행중

--- src/pages/Challenge/ChallengeMap.jsx
+++ src/pages/Challenge/ChallengeMap.jsx
@@ -4,16 +4,47 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 
 import { MdLocationOn } from "react-icons/md";
+import axios from "axios";
 import ChallengeInitTmap from "./ChallengeInitTmap";
 import ClickBottomModal from "../../components/common/Modal/ClickBottomModal";
-import { locationNameState } from "../../recoil/challenge";
+import {
+  currentLocationLatState,
+  currentLocationLongState,
+  locationLatState,
+  locationLongState,
+  locationNameState,
+} from "../../recoil/challenge";
+import userIdState from "../../recoil/userIdState";
+import CurrentLocation from "../../components/views/Challenge/CurrentLocation";
+
+function getCurrentDate() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
 
 const ChallengeMap = () => {
-  const navigate = useNavigate();
-
   const [modalOpen, setModalOpen] = useState(false);
+  const [challengeCheck, setChallengeCheck] = useState(false);
 
+  // 챌린지
   const [selectedPlace, setSelectedPlace] = useRecoilState(locationNameState);
+  const [selectedLocationLong, setSelectedLocationLong] = useRecoilState(locationLongState);
+  const [selectedLocationLat, setSelectedLocationLat] = useRecoilState(locationLatState);
+  const [userAuthState, setUserAuthState] = useRecoilState(userIdState);
+
+  // 내 현재좌표
+  const [currentLocationLong, setCurrentLocationLong] = useRecoilState(currentLocationLongState);
+  const [currentLocationLat, setCurrentLocationLat] = useRecoilState(currentLocationLatState);
+
+  const navigate = useNavigate();
+  const currentDate = getCurrentDate();
+  const url = `${process.env.REACT_APP_API_URL}/user/challenge/${userAuthState}`;
+
+  // console.log(url);
 
   const handleCloseModal = () => {
     setModalOpen(false);
@@ -31,13 +62,38 @@ const ChallengeMap = () => {
   };
 
   // 챌린지 완료 처리 함수
-  const handleChallengeComplete = () => {
+  const handleChallengeComplete = async () => {
+    /*  */
     setModalOpen(true);
     document.documentElement.style.overflow = "hidden";
 
     // 현재 GPS 값과 선택한 장소의 범위 반경을 비교하여 챌린지 성공 또는 실패 처리
     // 성공: showModalSuccess();
     // 실패: showModalFailure();
+
+    const body = {
+      name: selectedPlace,
+      /*   location: { lat: locationLat, long: locationLong }, */
+      check: challengeCheck,
+      user: userAuthState,
+      date: currentDate,
+    };
+    // console.log(body);
+
+    try {
+      const response = await axios.post(url, body);
+      if (response.status === 201) {
+        if (challengeCheck) {
+          alert("챌린지 성공");
+          setChallengeCheck(true);
+        } else {
+          alert("챌린지 실패 위치아님");
+          setChallengeCheck(false);
+        }
+      }
+    } catch (error) {
+      setChallengeCheck(false);
+    }
   };
 
   useEffect(() => {
@@ -50,27 +106,34 @@ const ChallengeMap = () => {
       <MapIcon className="icon-map" />
       <LocationContainer>
         <LocationIcon />
-        <LocationText> 현재 위치 | 00구 00동</LocationText>
+        <LocationText>
+          {" "}
+          현재 위치 | 00구 00동
+          <CurrentLocation />
+        </LocationText>
       </LocationContainer>
 
       <ChallengeInitTmap selectedPlace={selectedPlace} />
 
       <CompleteButton onClick={handleChallengeComplete}>도착 완료</CompleteButton>
 
-      <ClickBottomModal
-        modalText="해당 위치에서 다시 시도해주세요."
-        buttonLeftText="닫기"
-        modalOpen={modalOpen}
-        buttonLeftOnClick={handleCloseModal}
-      />
-      <ClickBottomModal
-        modalText="챌린지 성공!"
-        buttonLeftText="홈으로 가기"
-        buttonRightText="감정일기 쓰기"
-        modalOpen={modalOpen}
-        buttonLeftOnClick={handleClickHome}
-        buttonRightOnClick={handClickDiary}
-      />
+      {challengeCheck ? (
+        <ClickBottomModal
+          modalText="챌린지 성공!"
+          buttonLeftText="홈으로 가기"
+          buttonRightText="감정일기 쓰기"
+          modalOpen={modalOpen}
+          buttonLeftOnClick={handleClickHome}
+          buttonRightOnClick={handClickDiary}
+        />
+      ) : (
+        <ClickBottomModal
+          modalText="해당 위치에서 다시 시도해주세요."
+          buttonLeftText="닫기"
+          modalOpen={modalOpen}
+          buttonLeftOnClick={handleCloseModal}
+        />
+      )}
     </MapBox>
   );
 };
