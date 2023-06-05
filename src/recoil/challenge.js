3cbd9a8e86107c0a8f53bad8582f719f3e699865
hyeji07 <zindex_ann@naver.com>
2023-06-05 18:25:08 +0900

    feat: recoil 챌린지 현재 좌표 state 추가

--- src/recoil/challenge.js
+++ src/recoil/challenge.js
@@ -9,7 +9,17 @@ export const locationLongState = atom({
   key: "locationLongState",
   default: "",
 });
+
 export const locationLatState = atom({
   key: "locationLatState",
+  default: "",
+});
+
+export const currentLocationLongState = atom({
+  key: "currentLocationLongState",
+  default: "",
+});
+export const currentLocationLatState = atom({
+  key: "currentLocationLatState",
   default: "",
 });
