a9a9b8370f71408abe6e3d3d41cbc6565f95e3ba
yxxnhx <tjdbsgk514@hanmail.net>
2023-06-06 02:19:18 +0900

    커스텀 훅 usePosition 생성

--- src/hooks/usePosition.jsx
+++ src/hooks/usePosition.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
+
 const usePosition = () => {
-  return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        resolve(position);
-      },
-      error => {
-        reject(error);
-      },
-    );
-  });
+  const [position, setPosition] = useState(null);
+  const [error, setError] = useState(null);
+
+  const getPosition = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          setPosition(pos);
+          resolve(pos);
+        },
+        err => {
+          setError(err);
+          reject(err);
+        },
+      );
+    });
+  };
+
+  return { position, error, getPosition };
 };
 
 export default usePosition;
