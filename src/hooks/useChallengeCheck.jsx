d3eafa60e1903f4c5f71876a70e49655ae9a563e
Jiyeong Jeong <8533283@naver.com>
2023-06-06 11:45:26 +0900

    feat: challenge check 구현

--- src/hooks/useChallengeCheck.jsx
+++ src/hooks/useChallengeCheck.jsx
@@ -15,7 +15,7 @@ const useChallengeCheck = () => {
       const date = new Date();
       const year = date.getFullYear();
       const month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-      const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate() + 1}`;
+      const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`;
 
       const today = `${year}-${month}-${day}`;
 
